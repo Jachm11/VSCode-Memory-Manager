8f20f19682815d1d2360c4cfc2027dca7ddefc41
Jachm <joalchama@gmail.com>
2020-06-01 23:21:04 -0600

    Revert "Limpieza folder"
    This reverts commit 768b9e8be00981ddba9dd6078054db38614e90e6.

---
+++ Memory-Manger-Lib/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "GarbageCollector.h"
+
+using namespace std;
+
+void print(int x){
+
+    //std::cout<< x << std::endl;
+
+}
+
+void print(char* x){
+
+    //std::cout<< x << std::endl;
+
+}
+
+int main(int argc, char const *argv[])
+{
+    //LinkedList<int> hola;
+    //LinkedList<int> hola2;
+    //hola.insertFirst(10);
+    //std::cout<< "hola" << std::endl;
+    //std::cout<< hola2.getHead() << std::endl;
+    //int var = hola.getHead()->getData();
+    //print(var);
+    //print(hola.searchByIndex(1)->getData());
+    //print(hola.getSize());
+    //cout << "Puntero1" << endl;
+    //VSPtr<int> myPtr= VSPtr<int>::New();
+    //int var =34;
+    //myPtr.dato = &var;
+    //myPtr.hola();
+    //cout << "desdemain referencia: "<< addressof(myPtr) << endl;
+    //cout << "desdemain valor: " <<  &myPtr << endl;
+
+
+
+    (GarbageCollector :: GCInit());
+
+
+    
+    
+    return 0;
+}
